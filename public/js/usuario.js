9edd5f66f42de31e7230381a44a4e26e773af022
MateusLouAlmeida <mateus.almeida@sptech.school>
2026-04-29 22:07:10 -0300

    atualizando cadastro e login

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -1,51 +1,18 @@
 
+let nome_padrao = 'Fernando';
 let email_padrao = "brandao@email.com";
 let senha_padrao = "urubu100";
-let funcionario_padrao = "Miguel";
-let perm_funcionario_padrao = 1;
-let nome_estufa_padrao = "moranguinho"
-let min_ppm_padrao = 600
-let max_ppm_padrao = 900
+let cpf_padrao = '12345678910'
 
+let nomes = [nome_padrao];
 let emails = [email_padrao];
 let senhas = [senha_padrao];
-let funcionarios = [funcionario_padrao];
-let perm_funcionarios = [perm_funcionario_padrao];
-let nome_estufas = [nome_estufa_padrao];
-let min_ppms = [min_ppm_padrao];
-let max_ppms = [max_ppm_padrao];
-
-
-function cadastrarfunc() {
-    let nome_funcionario = ipt_funcionario.value;
-    let perm_funcionario = tipo_usuario.value;
-    funcionarios.push(funcionario_padrao);
-    perm_funcionarios.push(perm_funcionario_padrao);
-    alert("Funcionário cadadastrado");
-    ipt_funcionario.value = "";
-}
-
-
-function cadastrarestufa() {
-    let nome_estufa = ipt_estufa.value;
-    let min_ppm = ipt_minimo.value;
-    let max_ppm = ipt_max.value;
-    if (min_ppm < max_ppm) {
-
-        nome_estufas.push(nome_estufa_padrao);
-        min_ppms.push(min_ppm_padrao);
-        max_ppms.push(max_ppm_padrao);
-        alert("Estufa cadadastrada");
-    }
-    else {
-        alert("ppm mínimo maior que máximo");
-    }
-}
-
+let cpfs = [cpf_padrao];
 
 function cadastrar() {
     let nome_usuario = ipt_nome.value;
     let email_cadastro = ipt_email_criado.value;
+    let cpf = ipt_cpf.value;
     let senha_cadastro = ipt_senha_criada.value;
     let senha_confirmacao = ipt_c_senha.value;
 
@@ -61,8 +28,10 @@ function cadastrar() {
     else {
         alert("Usuário cadastrado");
 
-        emails.push(email_padrao);
-        senhas.push(senha_padrao);
+        nome_usuario.push(nomes)
+        emails.push(emails);
+        senhas.push(senhas);
+        cpfs.push(cpf);
     }
 
     ipt_nome.value = "";
