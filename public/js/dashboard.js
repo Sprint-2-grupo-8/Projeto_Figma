381c81f03f59be8751bad7590952a34db862732d
arthursptech <arthur.nmartins@sptech.school>
2026-06-04 13:34:45 -0300

    Finalizando integração das estufas individuais com o painel de alertas

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,9 @@
 let data_atual = new Date();
 let data_formatada = data_atual.toLocaleDateString('pt-BR');
-
+console.log(
+    'ID recebida:',
+    sessionStorage.getItem('ID_ESTUFA_FILTER')
+);
 const dataAtualElement = document.getElementById('data_atual');
 if (dataAtualElement) {
     dataAtualElement.innerText = data_formatada;
@@ -12,14 +15,24 @@ const grafico_individual = document.querySelectorAll('.individual');
 const indicadores_gerais = document.querySelector('.indicadores');
 const indicadores_individuais = document.querySelector('.indicadores-individual');
 
-function filter() {
+function filter(idEstufa) {
+    console.log('filter chamado com:', idEstufa);
     const tipo_dash = document.getElementById('filter');
     const escolha = tipo_dash.querySelector('option:checked');
-    const estufa_escolhida = escolha.dataset.estufa;
+    const estufa_escolhida = escolha ? escolha.dataset.estufa : undefined;
     // se estufa_escolhida == undefined, signfica que nenhuma estufa individual foi escolhida
-    
 
-    if (tipo_dash.value === 'geral') {
+    if (idEstufa != undefined) {
+        tipo_dash.value = 'individual';
+
+        let option_alerta = tipo_dash.querySelector(
+            `option[data-estufa="${idEstufa}"]`
+        );  
+            option_alerta.selected = true;
+
+    }
+
+    if (tipo_dash.value === 'geral' || !(idEstufa || estufa_escolhida)) {
 
         for (let i = 0; i < grafico_geral.length; i++) {
             grafico_geral[i].style.display = 'flex';
@@ -32,7 +45,8 @@ function filter() {
         indicadores_gerais.style.display = 'flex';
         indicadores_individuais.style.display = 'none';
 
-    } else if (tipo_dash.value === 'individual') {
+    } else if (tipo_dash.value === 'individual' || (idEstufa || estufa_escolhida)) {
+        console.log('Entrou na visão individual');
 
         for (let i = 0; i < grafico_geral.length; i++) {
             grafico_geral[i].style.display = 'none';
@@ -45,9 +59,8 @@ function filter() {
         indicadores_gerais.style.display = 'none';
         indicadores_individuais.style.display = 'flex';
 
-        obterDadosGrafico(estufa_escolhida);
-        buscarRegistros(estufa_escolhida);
+        obterDadosGrafico((idEstufa || estufa_escolhida));
+        buscarRegistros((idEstufa || estufa_escolhida));
     }
 }
-
 
--- public/js/painel.js
+++ public/js/painel.js
@@ -62,6 +62,7 @@ let estufas_alertas = JSON.parse(sessionStorage.getItem('ESTUFAS_ALERTAS')) || [
     { "estufa": 4, "alertas": 0, "alertas_estabilizados": 0 }
 ]; 
 
+// Faz a soma de todos os alertas no JSON
 let alertas_totais = 0;
 estufas_alertas.forEach(e => {
     alertas_totais += e.alertas;
@@ -77,6 +78,7 @@ async function atualizarAlertas() {
 
     areaNotificacoes.innerHTML = '';
 
+    // Reseta os alertas para nâo acumular infinitamente
     estufas_alertas.forEach(estufa => {
         estufa.alertas = 0;
     });
@@ -133,7 +135,7 @@ async function atualizarAlertas() {
                         <div class="descricao">
                             <span class="estufa">${textoStatus}: Estufa ${idEstufa} - ${ppm} ppm</span>
                             <span class="horario">Agora</span>
-                            <a href="dashboard.html"><i class="fa-solid fa-right-to-bracket"></i> Verificar a estufa</a>
+                            <button onclick="irParaDash(${idEstufa})"><i class="fa-solid fa-right-to-bracket"></i> Verificar a estufa</button>
                         </div>
                         <div class="buttons">
                             <button><img src="assets/img/check.svg" alt=""/></button>
@@ -152,3 +154,10 @@ async function atualizarAlertas() {
 
 // As notificações de alerta são geradas automaticamente, se baseia na leitutra mais recente de cada estufa e nos limites configurados;
 window.addEventListener('load', atualizarAlertas);
+
+function irParaDash(idEstufa) {
+    sessionStorage.setItem('ID_ESTUFA_FILTER', idEstufa);
+    console.log("estufa clicada", idEstufa);
+
+    window.location="dashboard.html";
+}
